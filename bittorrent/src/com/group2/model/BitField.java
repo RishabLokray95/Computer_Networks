c640576883d7d127964656ff7aa326355bc557db
Jain, Siddharth <jain.sid.94@gmail.com>
2020-11-02 02:23:00 -0500

    Refactored code

--- bittorrent/src/com/group2/model/BitField.java
+++ bittorrent/src/com/group2/model/BitField.java
@@ -1,47 +1,51 @@
 package com.group2.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BitField implements Serializable {
 
-    private AtomicBoolean[] bitFieldMessage;
+    private AtomicBoolean[] bitField;
     private boolean isEmpty = true;
+    private Integer haveBitsCount = 0;
 
-    public BitField(Integer payloadSize, Boolean hasFile) {
-        this.bitFieldMessage = new AtomicBoolean[payloadSize];
+    BitField(Integer payloadSize, Boolean hasFile) {
+        this.bitField = new AtomicBoolean[payloadSize];
 
         if (hasFile) {
             this.isEmpty = false;
-            for(int i = 0; i < bitFieldMessage.length; i++)
-                bitFieldMessage[i] = new AtomicBoolean(true);
+            for(int i = 0; i < bitField.length; i++)
+                bitField[i] = new AtomicBoolean(true);
         }else {
-            for(int i = 0; i < bitFieldMessage.length; i++)
-                bitFieldMessage[i] = new AtomicBoolean();
+            for(int i = 0; i < bitField.length; i++)
+                bitField[i] = new AtomicBoolean();
         }
     }
     //use setter maybe?
-    public AtomicBoolean[] getBitFieldMessage() {
-        return bitFieldMessage;
+    public AtomicBoolean[] getBitField() {
+        return bitField;
     }
 
     public AtomicBoolean hasBitIndex(Integer index) {
-        return bitFieldMessage[index];
+        return bitField[index];
     }
 
-    public void setBitField(Integer index){
+    public synchronized void setBit(Integer index){
         this.isEmpty = false;
-        bitFieldMessage[index].set(true);
+        this.haveBitsCount += 1;
+        bitField[index].set(true);
     }
 
-    public void setBitField(AtomicBoolean[] bitField){
+    public synchronized void setBit(AtomicBoolean[] bitField){
         this.isEmpty = false;
-        this.bitFieldMessage = bitField;
+        this.haveBitsCount = (int) Arrays.stream(bitField).filter(AtomicBoolean::get).count();
+        this.bitField = bitField;
     }
 
     public boolean isInteresting(BitField peerBitField){
-        for(int i=0 ; i < bitFieldMessage.length ; i++){
-            if(bitFieldMessage[i].get() && !peerBitField.bitFieldMessage[i].get()) {
+        for(int i = 0; i < bitField.length ; i++){
+            if(bitField[i].get() && !peerBitField.bitField[i].get()) {
                 return true;
             }
         }
@@ -49,8 +53,8 @@ public class BitField implements Serializable {
     }
 
     public Integer getInterestingFieldIndex(BitField peerBitField){
-        for(int i=0 ; i < bitFieldMessage.length ; i++){
-            if(bitFieldMessage[i].get() && !peerBitField.bitFieldMessage[i].get()) {
+        for(int i = 0; i < bitField.length ; i++){
+            if(bitField[i].get() && !peerBitField.bitField[i].get()) {
                 return i;
             }
         }
@@ -59,5 +63,9 @@ public class BitField implements Serializable {
     
     public boolean isEmpty() {
         return isEmpty;
+    }
+
+    public Integer haveBitsCount() {
+        return haveBitsCount;
     }
 }
